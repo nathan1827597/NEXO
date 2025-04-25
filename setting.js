9f81aa7183369b49266a66d4bc2e5b3a1a464e3d
nathan1827597 <hartawinata440@gmail.com>
2025-04-25 13:48:12 +0700

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,1 +1,474 @@
+const { version } = require("./package.json")
 
+// Settings Bot 
+global.owner = '6283170213967'
+global.versi = version
+global.namaOwner = "KyzzDev"
+global.packname = 'Bot WhatsApp By Kyzz Offc'
+global.botname = 'KyzzBotz'
+global.botname2 = 'Kyzz-Host'
+global.tempatDB = 'database.json' // Jangan ubah
+global.pairing_code = true // Jangan ubah
+
+// Settings Link / Tautan
+global.linkOwner = "https://wa.me/6283170213967"
+global.linkGrup = "https://chat.whatsapp.com/LA3sgmFOsck4l5h3OarwaU"
+
+// Delay Jpm & Pushctc || 1000 = 1detik
+global.delayJpm = 3000
+global.delayPushkontak = 6000
+
+// Settings Channel / Saluran
+global.linkSaluran = "https://whatsapp.com/channel/0029Vb3qE78BadmWhAfscZ3I"
+global.idSaluran = "120363374835632700@newsletter"
+global.namaSaluran = "𝗜𝗻𝗳𝗼𝗿𝗺𝗮𝘁𝗶𝗼𝗻 𝗔𝗯𝗼𝘂𝘁 𝗞𝘆𝘇𝘇𝘇 ✯"
+global.pinH2H = "-"
+global.passwordH2H = "-"
+global.merchantIdOrderKuota = "OK1934316"
+global.apiOrderKuota = "445145517394876141934316OKCT8C7F98A06B1D83E1FCEF98243126C38F"
+global.qrisOrderKuota = "00020101021126670016COM.NOBUBANK.WWW01189360050300000879140214500181052837730303UMI51440014ID.CO.QRIS.WWW0215ID20243329426790303UMI5204541153033605802ID5919ALI STORE OK19343166012TAPANULI SEL61052273262070703A016304DD5B"
+
+// Settings Api Digital Ocean
+global.apiDigitalOcean = "-"
+
+// Settings Api Digital Ocean
+global.apiSimpelBot = "simplebotz85"
+
+
+// Settings All Payment
+global.shopepay = "Tidak Tersedia"
+global.dana = "083176793414"
+global.ovo = "Tidak Tersedia"
+global.gopay = "083176793414"
+
+// Settings Image Url
+global.image = {
+menu: "https://files.catbox.moe/fszmhf.jpg", 
+reply: "https://files.catbox.moe/fszmhf.jpg", 
+logo: "https://files.catbox.moe/fszmhf.jpg", 
+dana: "https://img102.pixhost.to/images/79/556578147_biyu.jpg", 
+ovo: "https://img102.pixhost.to/images/79/556578147_biyu.jpg", 
+gopay: "https://img102.pixhost.to/images/79/556578147_biyu.jpg", 
+qris: "https://files.catbox.moe/qjhbf9.jpg"
+}
+
+// Settings Api Panel Pterodactyl
+global.egg = "15" // Egg ID
+global.nestid = "5" // nest ID
+global.loc = "1" // Location ID
+global.domain = "https://kyzz-pengen-balikan.sevs.my.id"
+global.apikey = "ptla_iJx8TokTBvFG6hHkITUcbvTT88P7lf8Hbfet5hPMxAS" //ptla
+global.capikey = "ptlc_Qmmxs0v4pFPER4lDVFuJTKUYUJr7tbc8WR7ZFpvA7fK" //ptlc
+
+// Settings Api Panel Pterodactyl Server 2
+global.eggV2 = "15" // Egg ID
+global.nestidV2 = "5" // nest ID
+global.locV2 = "1" // Location ID
+global.domainV2 = "https://-"
+global.apikeyV2 = "-" //ptla
+global.capikeyV2 = "-" //ptlc
+
+// Settings Api Subdomain
+global.subdomain = {
+"rizxvelz.online": {
+"zone": "8eac5133e31d86027c631df0bccb9a05", 
+"apitoken": "JAU5FRMKWDykRWXQq3lre4PzRjJUVLgbw1VfcEnv"
+},
+"rizxvelz.shop": {
+"zone": "13cc550eaa6101a6865886e7b0c4be30", 
+"apitoken": "DJ8xCUS1EmaO_e5RaagMXpaPP5asaVH0Tc9DHD7w"
+},
+"kenz-host.my.id": {
+"zone": "df24766ae8eeb04b330b71b5facde5f4", 
+"apitoken": "fyaxLxD0jNONtMWK3AmnaiLkkWi5Wg3Y9h8nqJh6"
+},
+"panelkishop.web.id": {
+"zone": "8f4812b3c78ca478b5d162b6cb35d1b3", 
+"apitoken": "3Y0cW3cVVIhyeWHytqFEbGDrdWaAC-k8twOEeFP2"
+},
+"tokopanelkishop.biz.id": {
+"zone": "d87d4f320d9902f31fbbcc5ee23fafe8", 
+"apitoken": "D00akOLxF3qzBzpYBp5SbpaLTmwYeybNsyAcDfiB"
+},
+"rikionline.shop": {
+"zone": "082ec80d7367d6d4f7c52600034ac635", 
+"apitoken": "r3XUyNYtxNQYwZtGUIAChRqe0uTzwV4eVO7JpJ_l"
+},
+"sevsbotz.xyz": {
+"zone": "594ebd64574548c2cdda04f500059bf3", 
+"apitoken": "IDjXUZTsJejkc4hGIU3j-_7h7_i531MxOlb3drVf"
+}, 
+"lexczalok.xyz": {
+"zone": "dd510b41fc4d7074c5be6f47f9f5b722", 
+"apitoken": "IsRLdOOP7OVrB95PUWaW_eq1n5T2T8OUcnwGhP_q"
+}, 
+"lexcz.xyz": {
+"zone": "3285d8510ee8f4859cfdf533247cf301", 
+"apitoken": "7NwxFTvoWspSD4xzCFCzlvFoV0-D7-qx9rwvCSf"
+}, 
+"serverlex.xyz": {
+"zone": "e3f263f43641ef8e6a6261c2cca38148", 
+"apitoken": "0LpR6Nad8JxAAWp5dRwp0X5g4JrdeusG2EmsGVOa"
+}, 
+"czteam.my.id": {
+"zone": "487f88c7bdf9fddd9e8a8dcd68e0ad73", 
+"apitoken": "itkLTdhJochwxOIlKh5_8vLhxgQWqfLHo9KnuyLm"
+}, 
+"lexcz.me": {
+"zone": "7a4e7ca1131daf5a4c7ef03191432a6a", 
+"apitoken": "DTxnQFaoI9p2YtZUL7PLikauBvXcL_CWzpBbQx2b"
+},
+"zyydev.my.id": {
+"zone": "337aaf9a6689c7a7145480ef3ccaffdb", 
+"apitoken": "jnNO465SjNC-Ss6CDM2WDIy7jwzbKWHJuOXA5xak"
+},
+"chizyy.my.id": {
+"zone": "057cbf622eed270982769d5557dcee59",
+"apitoken": "BUg6UBu_68M1fG21nD6QzZkkpa9_Zp3hP54LsxX3",
+},
+"sevscaw.xyz": {
+"zone": "51bb979d4ce04104ec42bf17f5060c94",
+"apitoken": "SfOcZTyF0_3WnsWK8Chfqn5OJweMq8vPZzwGxjOm",
+},
+"jstpiwz.my.id": {
+"zone": "f1901becfbd79f39048f7698de71d53b",
+"apitoken": "g8_D70UKwk0hBeuPqdXgWmZcoNjwXMkfd3OEUL4k",
+},
+"piwzaja.my.id": {
+"zone": "9df8ff41e6a168624dafe10f3a9064fa",
+"apitoken": "LyPlcMS9pqKhqPc0HfkKS54OovoQw57YmmGzdO87",
+},
+"bokep.biz.id": {
+"zone": "9b480676b96b2b2a9ed1802c66717e99",
+"apitoken": "Poj4qdb4rudfu0dPz1G6_XHg-Qm1FQEdwM8iXAHt",
+},
+"kyzzoffc.my.id": {
+"zone": "adda32e445cc447949d7132f83dfe1eb",
+"apitoken": "nVJwe1fac4cb3IbkOIJnmsJvt0DQqI2leRTtoWex",
+},
+"kyzzz.biz.id": {
+"zone": "fde74eba4305ca2e735226da380851a3",
+"apitoken": "beOZOBHMs9ea_KnBlU40wmaerJpitU5KmadjPgqk",
+},
+"hitam.systems": {
+"zone": "cfaba3ef0065acc82facc17f1b79d50b",
+"apitoken": "4o3tZAc5jOzA00joKpLYhG_616qCJxNLGlhawMEY",
+},
+"plercicak.biz.id": {
+"zone": "1e54ebb127f0f1cf91f4a335e3e628f2",
+"apitoken": "uu0Dj6iI4949j5cJB5NbwF2T1FdksO98Wk-B003D",
+},
+"privatehost.us.kg": {
+"zone": "790918217c4add75b7684458518c5836", 
+"apitoken": "qYv4NvEN6ZcUIv4dEXihjkmQMwbP_-3Qy_zFlAHv"
+}, 
+"botwhatsapp.us.kg": {
+"zone": "fb1ac418c5564373a56c91d962b30dca", 
+"apitoken": "rfQih0XNXiq7AyEuDoLjoFfHX2mhYf_9kddAdKIo"
+}, 
+"skyzopedia.us.kg": {
+"zone": "9e4e70b438a65c1d3e6d0e48b82d79de", 
+"apitoken": "odilM9DpvLVPodbPyZwW7UcDKg1aIWsivJc0Vt_o"
+}, 
+"marketplace.us.kg": {
+"zone": "2f33118c3db00b12c38d07cf1c823ed1", 
+"apitoken": "6WS_Op6yuPOWcO17NiO-sOP8Vq9tjSAFZyAn82db"
+}, 
+"pteroserver.us.kg": {
+"zone": "f693559a94aebc553a68c27a3ffe3b55", 
+"apitoken": "ZPAXx7CL51PtbGweL2pE3BsI3x0hgTgLuy56iXuo"
+}, 
+"serverku.biz.id": {
+"zone": "4e4feaba70b41ed78295d2dcc090dd3a", 
+"apitoken": "oof_QRNdUC4aMQ3xIB8dmkGaZu7rk2J-0P_tN55l"
+}, 
+"privatserver.my.id": {
+"zone": "699bb9eb65046a886399c91daacb1968", 
+"apitoken": "CrQMyDn2fhchlGne2ogAw7PvJLsg4x8vasBv__6D"
+}, 
+"panelwebsite.biz.id": {
+"zone": "2d6aab40136299392d66eed44a7b1122", 
+"apitoken": "cj17Lzg9otqwkYIVzgL0pcVA4GfcXqePHAOhCqa_"
+}, 
+"mypanelstore.web.id": {
+"zone": "c61c442d70392500611499c5af816532", 
+"apitoken": "N_VhWv2ZK6UJxLdCnxMfZx9PtzAdmPGM3HmOjZR4"
+}, 
+"pteroserver.us.kg": {
+"zone": "f693559a94aebc553a68c27a3ffe3b55", 
+"apitoken": "qRxwgS3Kl_ziCXti2p4BHbWTvGUYzAuYmVM28ZEp"
+}, 
+"digitalserver.us.kg": {
+"zone": "df13e6e4faa4de9edaeb8e1f05cf1a36", 
+"apitoken": "sH60tbg10UH8gpNrlYpf3UMse1CNJ01EKJ69YVqb"
+}, 
+"biyu-offc.biz.id": {
+"zone": "486604cbd7c410b1d883ce35da2d95f6", 
+"apitoken": "lHSc0-55bsdaUKbXNuY1WUw7RVCPX51h2XMe3EY3"
+}, 
+"shopserver.us.kg": {
+"zone": "54ca38e266bfdf2dcdb7f51fd79c2db5", 
+"apitoken": "4qOupI-Of-6yNrBaeS1-H0KySuKCd0wS-x0P5XQ4"
+}, 
+"devzzhosting.my.id": {
+"zone": "f994c2f66ea24d81ea5f4a17a57a725f", 
+"apitoken": "AqzTJ13YE2ZxmKXdBP4imLhFMFPjN8VUt9mpbTGq"
+},
+"uchiha.tech": {
+"zone": "1b09e81ec29d760ff33e476a084de6ed",
+"apitoken": "nzLwv-2uzMt3Ihsd5MVV2aLJ9EoovxrVK7Y4b2To",
+},
+"buyer-vps.site": {
+"zone": "354e1c784bed5a8d93f458ec1ff86f35",
+"apitoken": "rzOLIO1c-Me1X0owStLFUZ0ggfs_cxHDab2A7krF",
+},
+"arzanoffc.online": {
+"zone": "43ca801f5c6a73a77da5f0243f4f8c1d",
+"apitoken": "RZQft_gJGOJDskx6L6SxREJCvOKw0hclLjHUCNLS",
+},
+"hexaneuro.biz.id": {
+"zone": "16273869fda58585fc435e4582a5ff56",
+"apitoken": "y4wV0um-bvrVTvFVPrNrGEi590bPowvosRyNiGMn"
+ },   
+"mypanel.tech": {
+"zone": "9c5460deca7c84273b46f2f047783636",
+"apitoken": "0IBfmlUlCkYqoFPWYaqCmqI3_vWxkrFNbxrQjzX2"
+}, 
+"kedai-panel.me": {
+"zone": "3ab91ea96368234719c9de7d260742e2",
+"apitoken": "O22A19W7trZZ52bAXtF77aUVdGDdAk8PjQqwM7qv"
+}, 
+"control-pannel.site": {
+"zone": "20c517d7dcc25d9cf516f5c4cf2d21ba",
+"apitoken": "mAA84Qs4nJvLTaup0HTGrpVbgaSk-4vfUrcdHgCH"
+}, 
+"mypanelstore.xyz": {
+"zone": "72f7362d7686d9158a8e389851fc379c", 
+"apitoken": "V2y-I2o1s5E1tdCPpUSnT4FokV6WuxEUC2FqqVIX"
+},
+"panel-pvrt.my.id": {
+"zone": "ee4060e03b223e8e0724908575b70c3b", 
+"apitoken": "6HxTQ0VJZVcAJISlKsezttCNzpAAf7ubPy5EDOGR"
+},
+"ekiofficial.my.id": {
+"zone": "df33365b44b11cbe51570a7ed981cae5", 
+"apitoken": "rMJGbyeuwFVZJifsB3rIX-nRpIOOa4Wkrhu7V5Jo"
+},
+"ekiofficial.web.id": {
+"zone": "e1b037c00268cae95076b58f7f78b1f6", 
+"apitoken": "EJO7mHrBORH9XoQrnUvBqotMYxNm5bjB5UO2PeQE"
+},
+"eki-panelpvrt.my.id": {
+"zone": "6b4cb792b77b6118e91d8604253ca572", 
+"apitoken": "DsftwwFCAKrbSo-9r9hxqcscMw8Xvx8gQzTXMSz4"
+},
+"panelvip.biz.id": {
+"zone": "70969a584446a244efe1461f5bb41ff5", 
+"apitoken": "pzj2mcvQL1KUQCjj5XjSVPjLqKGCy8U7PtVFuOXr"
+},
+"shop-panel.biz.id": {
+"zone": "62e0683ee057e0e2a39eaf73d18e6eb1", 
+"apitoken": "opUN9xsI7mlS3CP8cJA9RbO5SB4gUpcyof9Yaeb7"
+},
+"centzzcloud.my.id": {
+"zone": "749f1d7d69e9329195761b570010c00f", 
+"apitoken": "9Su8A1EDXnt9-yGDb7YSGlY_ogJAw2vR9IDtpFrQ"
+},
+"pakvinsen.me": {
+"zone": "3b8cb89265c0e026abaf3bc50ed57e76", 
+"apitoken": "ttt0IHK50UKP2HltWUauuyDzkVPqnOEkx7M-5CFs"
+},
+"satoruuhost.tech": {
+"zone": "086b2b4e1c65c22bfd8d5d86cbfa947f", 
+"apitoken": "62XZSKEzz4mKM_1wAHK7-_JaA14VrQJDoWI2GeHT"
+},
+"hostsatoruu.biz.id": {
+"zone": "30ea1aac05ca26dda61540e172f52ff4", 
+"apitoken": "eZp1wNcc0Mj-btUQQ1cDIek2NZ6u1YW1Bxc2SB3z"
+},
+"mafiapnel.my.id": {
+"zone": "34e28e0546feabb87c023f456ef033bf", 
+"apitoken": "bHNaEBwaVSdNklVFzPSkSegxOd9OtKzWtY7P9Zwt"
+},
+"rexxaoffc.my.id": {
+"zone": "f972ed410a833b28c8b5f166d6620d6a", 
+"apitoken": "liq8sBPHwvbU2jWQYTCjo4BXafVPeopkAU4avUlP"
+}, 
+"rexxa.my.id": {
+"zone": "b0d37cb6e9d6a7c2b0a82395cbdfd8b9", 
+"apitoken": "fR2LO4Hz2y0U8dP3IHRwMHnWi_xKKa5RCZjWaXv3"
+},
+"pteroserver.us.kg": {
+"zone": "f693559a94aebc553a68c27a3ffe3b55", 
+"apitoken": "ZPAXx7CL51PtbGweL2pE3BsI3x0hgTgLuy56iXuo"
+},
+"gacorr.biz.id": {
+"zone": "cff22ce1965394f1992c8dba4c3db539", 
+"apitoken": "v9kYfj5g2lcacvBaJHA_HRgNqBi9UlsVy0cm_EhT"
+},
+"cafee.my.id": {
+"zone": "0d7044fc3e0d66189724952fa3b850ce", 
+"apitoken": "wAOEzAfvb-L3vKYE2Xg8svJpHfNS_u2noWSReSzJ"
+}, 
+"store-panell.my.id": {
+"zone": "0189ecfadb9cf2c4a311c0a3ec8f0d5c", 
+"apitoken": "eVI-BXIXNEQtBqLpdvuitAR5nXC2bLj6jw365JPZ"
+}, 
+"vipstoree.my.id": {
+"zone": "72fd03404485ddba1c753fc0bf47f0b3", 
+"apitoken": "J2_c07ypFEaen92RMS7irszQSrgZ_VFMfgNgzmp0"
+}, 
+"bokepp.biz.id": {
+"zone": "46b8cab5631c6c23c5ec4a7ef1f10803", 
+"apitoken": "A8df8PxnKIcxLUTE7XS4TRZBoLslvt4XjJb1XEyi"
+}, 
+"sevs.my.id": {
+"zone": "d94d38a2880ff62768c41a48733fc6fd", 
+"apitoken": "ZbZQX-iDeN6k-6ju_gUBgq1dt10eXtAPnguE2IbJ"
+},
+"bagaskun-hostinger.my.id": {
+"zone": "15f32409228f2a281d9cf87930389b6b",
+"apitoken": "aERRV5Uc1K4lZkHCL1aQkwgwWi7pMvlo5J5JC6MM"
+},
+"sevsaja.biz.id": {
+"zone": "187e53e81285d5544c3b196831e14868",
+"apitoken": "DZvTf-wFU4TcswBnSa2LW2pv7aVE6dGfDfrR2tM4"
+},
+"wannhosting.biz.id": {
+"zone": "4e6fe33fb08c27d97389cad0246bfd9b",
+"apitoken": "75HrVBzSVObD611RkuNS1ZKsL5A_b8kuiCs26-f9"
+},   
+"wannhosting.my.id": {
+"zone": "0b36d11edd793b3f702e0591f0424339",
+"apitoken": "OsSjhDZLdHImYTX8fdeiP1wocKwVnoPw5EiI85IF"
+}, 
+"xnxxx.tech": {
+"zone": "639f9cde20c22b1d2f33b2fee54f8f59",
+"apitoken": "MtWI3a9-9Za-fGKmwl0uNznqM94eljKgobkF36h1"
+},
+"pterodactyl-panel.web.id": {
+"zone": "d69feb7345d9e4dd5cfd7cce29e7d5b0",
+"apitoken": "32zZwadzwc7qB4mzuDBJkk1xFyoQ2Grr27mAfJcB"
+},
+"panelprivate.web.id": {
+"zone": "61bcd80ff1ec9c3a5812f74d6ec24535",
+"apitoken": "VnjVDtbb-fSTFIn-3Hckd_E_eseqyHH7u1TTAHMN"
+},
+"digitalserver.biz.id": {
+"zone": "24dec03ff55093ab3a2d1dc926c372df", 
+"apitoken": "EnmTeuJUzFF_xmnDnTAlwdxajP5ogeJKH9wy-SiR"
+}, 
+"serverpanell.biz.id": {
+"zone": "aac5555d4f60fcf48c9e901b938fffc0", 
+"apitoken": "qd8OolI2_GIu3HLsTZvdk242ggDdD3_IGXMMSQ7P"
+}, 
+"privatehost.us.kg": {
+"zone": "790918217c4add75b7684458518c5836", 
+"apitoken": "qYv4NvEN6ZcUIv4dEXihjkmQMwbP_-3Qy_zFlAHv"
+}, 
+"botwhatsapp.us.kg": {
+"zone": "fb1ac418c5564373a56c91d962b30dca", 
+"apitoken": "rfQih0XNXiq7AyEuDoLjoFfHX2mhYf_9kddAdKIo"
+}, 
+"skyzopedia.us.kg": {
+"zone": "9e4e70b438a65c1d3e6d0e48b82d79de", 
+"apitoken": "odilM9DpvLVPodbPyZwW7UcDKg1aIWsivJc0Vt_o"
+}, 
+"marketplace.us.kg": {
+"zone": "2f33118c3db00b12c38d07cf1c823ed1", 
+"apitoken": "6WS_Op6yuPOWcO17NiO-sOP8Vq9tjSAFZyAn82db"
+}, 
+"pteroserver.us.kg": {
+"zone": "f693559a94aebc553a68c27a3ffe3b55", 
+"apitoken": "ZPAXx7CL51PtbGweL2pE3BsI3x0hgTgLuy56iXuo"
+}, 
+"digitalserver.us.kg": {
+"zone": "df13e6e4faa4de9edaeb8e1f05cf1a36", 
+"apitoken": "HXVf4soYFM3iiOewHZ6tk6LEnG9f7m7CVhU0EoVz"
+}, 
+"xyz-store.biz.id": {
+"zone": "8ae812c35a94b7bd2da993a777b8b16d", 
+"apitoken": "oqZafkd3mSt1bABD9MMTidpCtD9VZdiPTjElVKJB"
+}, 
+"shopserver.us.kg": {
+"zone": "54ca38e266bfdf2dcdb7f51fd79c2db5", 
+"apitoken": "4qOupI-Of-6yNrBaeS1-H0KySuKCd0wS-x0P5XQ4" 
+},
+"kenz-host.my.id": {
+"zone": "df24766ae8eeb04b330b71b5facde5f4", 
+"apitoken": "fyaxLxD0jNONtMWK3AmnaiLkkWi5Wg3Y9h8nqJh6"
+},
+"panelkishop.web.id": {
+"zone": "8f4812b3c78ca478b5d162b6cb35d1b3", 
+"apitoken": "3Y0cW3cVVIhyeWHytqFEbGDrdWaAC-k8twOEeFP2"
+},
+"tokopanelkishop.biz.id": {
+"zone": "d87d4f320d9902f31fbbcc5ee23fafe8", 
+"apitoken": "D00akOLxF3qzBzpYBp5SbpaLTmwYeybNsyAcDfiB"
+},
+"panelprivate.web.id": {
+"zone": "61bcd80ff1ec9c3a5812f74d6ec24535", 
+"apitoken": "VnjVDtbb-fSTFIn-3Hckd_E_eseqyHH7u1TTAHMN"
+},
+"rikionline.shop": {
+"zone": "082ec80d7367d6d4f7c52600034ac635", 
+"apitoken": "r3XUyNYtxNQYwZtGUIAChRqe0uTzwV4eVO7JpJ_l"
+},
+"market-panel.site": {
+"zone": "d06bf5450ae51612a400bab1c4450283", 
+"apitoken": "kmb6AkpJ6XvHMzw2m0KbYKZOycIURNYMPA7Wm0BE"
+}, 
+"admin-panel.tech": {
+"zone": "305d4757160a88486d3780785c7c9887", 
+"apitoken": "UH-aDtxu5Mm9oU8khMd5ZVg22f7nhnSkCORBn8zP"
+}, 
+"kang-panel.tech": {
+"zone": "28baf36eb9ced4be271bdb6ea3320f41", 
+"apitoken": "MBiKuyoS0e0zJc8LKUjSXyLmgAf1IV9u86FJmVny"
+}, 
+"buyer-vps.site": {
+"zone": "354e1c784bed5a8d93f458ec1ff86f35", 
+"apitoken": "rzOLIO1c-Me1X0owStLFUZ0ggfs_cxHDab2A7krF"
+}, 
+"shopserver.us.kg": {
+"zone": "54ca38e266bfdf2dcdb7f51fd79c2db5", 
+"apitoken": "4qOupI-Of-6yNrBaeS1-H0KySuKCd0wS-x0P5XQ4"
+},
+"tokopanel.software": {
+"zone": "cc9638d4c289130ba070484625e6aefa", 
+"apitoken": "bcAP5z3gHvmQTlQ59qNB9BWmf0JiGt0C99FU6SXs"
+},
+"digitalserver.us.kg": {
+"zone": "df13e6e4faa4de9edaeb8e1f05cf1a36", 
+"apitoken": "HXVf4soYFM3iiOewHZ6tk6LEnG9f7m7CVhU0EoVz"
+},
+"pteroserver.us.kg": {
+"zone": "f693559a94aebc553a68c27a3ffe3b55", 
+"apitoken": "ZPAXx7CL51PtbGweL2pE3BsI3x0hgTgLuy56iXuo"
+},
+"cloud-shopp.biz.id": {
+zone: "365f57282cbea3a6d5a738f107df244e",
+apitoken: "hZKxD6afDLF-wsg1qVA-qbDK_h8lBE4NtqnVZPP8"
+}
+}
+
+// Message Command 
+global.mess = {
+	owner: "* Akses Ditolak\nFitur ini hanya untuk owner bot!",
+	admin: "* Akses Ditolak\nFitur ini hanya untuk admin grup!",
+	botAdmin: "* Akses Ditolak\nFitur ini hanya untuk ketika bot menjadi admin!",
+	group: "* Akses Ditolak\nFitur ini hanya untuk dalam grup!",
+	private: "* Akses Ditolak\nFitur ini hanya untuk dalam private chat!",
+	prem: "* Akses Ditolak\nFitur ini hanya untuk user premium!",
+	wait: 'Loading...',
+	error: 'Error!',
+	done: 'Done'
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(Update ${__filename}))
+	delete require.cache[file]
+	require(file)
+})
